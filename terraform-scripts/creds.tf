9c6dd2bd6c52cf80a55a11ea11e4d20ff851c53d
usridzero <usridzero@gmail.com>
2021-01-17 17:34:29 +0000

    aws fix.

--- terraform-scripts/creds.tf
+++ terraform-scripts/creds.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   access_key = "ASIAR3MU7DJXQE6YMTWY"
   secret_key = "CHiyiOe5fHH4nZUc6+w8IilvTBW0y1CrMAOcp2tv"
+  security_token = "FwoGZXIvYXdzEH8aDBI4vts/CQa5fnNG6SKzAXQnPku6+wmakldlXGK34rhgta2TRV4zRuer0g6T1RWzHXwjT6UejJ4fSkeWgYsQl7ksikbg3TV8/bpUGuH9nuMz2x3GsMWGE4DoxcRkFbg+bqwiUEWfFZ+T0y+4+xVjjW3Q34//neAtcQyvDjJc4qPVAnFO60EiQuJfrIWVztiuIpVN1sa8i+wKjoOWxN/dzNN+EjvTKr9WKwDByDGBWIkmT2usQ0HxlBYtZHLOSg0UNqknKK2HkYAGMi25vP38etwqazQ7Yt8F6yWQrBESbmdgNulEBy/5NWRHbd6yOVApwkUSBZc9Bho="
   region = "us-east-1"
 }
